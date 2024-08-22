6d8e8ec7714eb369ad2615c6ab615d0e39cc1a36
Athan Clark <athan.clark@gmail.com>
2024-08-22 11:15:03 -0700

    using ints for ids

--- src/Lib/Types/Id.hs
+++ src/Lib/Types/Id.hs
@@ -17,62 +17,28 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 You can reach me at athan.clark@gmail.com.
 -}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Lib.Types.Id (GroupId, ActorId, SpaceId, EntityId, VersionId) where
 
-import Control.Monad (replicateM)
-import Data.Aeson (FromJSON (parseJSON), ToJSON (toJSON), Value (String))
-import Data.Aeson.Types (typeMismatch)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Base16 as BS16
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Hashable (Hashable)
-import qualified Data.Text.Encoding as T
-import Data.Word (Word8)
 import GHC.Generics (Generic)
 import System.Random.Stateful (Uniform (uniformM))
-import Test.QuickCheck (Arbitrary (arbitrary), vectorOf)
-import Test.QuickCheck.Gen (chooseUpTo)
+import Test.QuickCheck (Arbitrary (arbitrary))
+import Test.QuickCheck.Gen (chooseInt)
 import Test.QuickCheck.Instances ()
-import Text.Read (Read (readPrec))
 
 -- | 12 byte random number
 newtype Id = Id
-  { getId :: BS.ByteString
+  { getId :: Int
   }
-  deriving (Eq, Ord, Hashable, Generic)
-
-instance Show Id where
-  show (Id x) = show (BS16.encode x)
-
-instance Read Id where
-  readPrec = do
-    bs <- readPrec
-    case BS16.decode bs of
-      Left e -> fail e
-      Right x -> pure (Id x)
-
-instance ToJSON Id where
-  toJSON (Id x) = String (T.decodeUtf8 (BS16.encode x))
-
-instance FromJSON Id where
-  parseJSON (String x) = case BS16.decode (T.encodeUtf8 x) of
-    Left e -> fail e
-    Right x -> pure (Id (x))
-  parseJSON json = typeMismatch "Id" json
+  deriving (Show, Read, ToJSON, FromJSON, Eq, Ord, Hashable, Generic)
 
 instance Uniform Id where
-  uniformM gen = do
-    xs <- replicateM 12 (uniformM gen)
-    pure (Id (BS.pack xs))
+  uniformM gen = Id <$> uniformM gen
 
 instance Arbitrary Id where
-  arbitrary = do
-    xs :: [Word8] <-
-      vectorOf 12 (fmap (fromIntegral . (`mod` 256)) (chooseUpTo maxBound))
-    pure (Id (BS.pack xs))
+  arbitrary = Id <$> chooseInt (minBound, maxBound)
 
 newtype GroupId = GroupId
   { getGroupId :: Id
